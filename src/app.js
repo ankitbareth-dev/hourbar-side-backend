70098262314bdf831299cb752f691a851508696b
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-05-14 02:57:54 +0530

    add login page with ejs

--- src/app.js
+++ src/app.js
@@ -2,4 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
+app.get("/", (req, res) => {
+  res.render("login", { error: "" });
+});
+
 export default app;
